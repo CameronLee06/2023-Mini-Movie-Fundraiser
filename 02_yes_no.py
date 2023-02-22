eb6cf0d119bf245e94b4973003e9f7cc8e09a873
CameronLee06 <99371789+CameronLee06@users.noreply.github.com>
2023-02-22 14:57:29 +1300

    Add files via upload

---
+++ 02_yes_no.py
@@ -0,0 +1,28 @@
+# functions go here
+def yes_no(question):
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+
+        elif response == "no" or response == "n":
+            return "no"
+
+        else:
+            print("Please enter yes or no")
+
+
+# main routine go here
+
+while True:
+    want_instructions = input("Do you want to read the instructions?").lower()
+
+    if want_instructions == "yes" or want_instructions == "y":
+        print("Instructions go here")
+    elif want_instructions == "no" or want_instructions == "n":
+        pass
+    else:
+        print("please answer yes / no")
+
+    print("we are done")
