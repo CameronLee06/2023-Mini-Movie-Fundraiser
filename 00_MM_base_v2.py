a230aa746f45c7a085a7d58a4c31eac14793c8ac
CameronLee06 <99371789+CameronLee06@users.noreply.github.com>
2023-03-14 10:01:08 +1300

    Add files via upload

--- 00_MM_base_v2.py
+++ 00_MM_base_v2.py
@@ -1,5 +1,6 @@
 import pandas
-
+import random
+from datetime import date
 
 # functions go here
 
@@ -63,8 +64,8 @@ def calc_ticket_price(var_age):
     return price
 
 
-# checks that users enter a valid response (eg yes / no)
-# cash / credit) based on a list of options
+# checks that users enter a valid response (eg. yes / no)
+# (cash / credit) based on a list of options
 def string_checker(question, num_letters, valid_responses, ):
     error = "Please choose {} or {}".format(valid_responses[0],
                                             valid_responses[1])
@@ -153,7 +154,54 @@ while tickets_sold < MAX_TICKETS:
     all_ticket_costs.append(ticket_cost)
     all_surcharge.append(surcharge)
 
+# create data frame from dictionary to organise information
+mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+mini_movie_frame = mini_movie_frame.set_index('Name')
 
+# Calculate the total ticket cost (ticket + surcharge)
+mini_movie_frame['Total'] = mini_movie_frame['Surcharge'] \
+                            + mini_movie_frame['Ticket Price']
+
+# calculate the profit for each ticket
+mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
+
+# calculate ticket and profit totals
+total = mini_movie_frame['Total'].sum()
+profit = mini_movie_frame['Profit'].sum()
+
+# Currency Formatting (uses currency function)
+add_dollars = ['Ticket Price', 'Surcharge', 'Total', 'Profit']
+for var_item in add_dollars:
+    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
+
+# choose a winner from our name list
+winner_name = random.choice(all_names)
+
+# get position of winner name in list
+win_index = all_names.index(winner_name)
+
+# look up total amount won (ie: ticket price + surcharge)
+total_won = mini_movie_frame.at[win_index, 'Total']
+
+print("---- Ticket Data ----")
+print()
+
+# output table with ticket data
+print(mini_movie_frame)
+
+print()
+print("----- Ticket Cost / Profit -----")
+
+# output total ticket sales and profit
+print("Total Ticket Sales: ${:.2f}".format(total))
+print("Total Profit : ${:.2f}".format(profit))
+
+print()
+print('---- Raffle Winner ----')
+print("Congratulations {}. You have won ${} ie: your "
+      "ticket is free!").format(winner_name, total_won)
+
+print()
 # output number of tickets sold
 if tickets_sold == MAX_TICKETS:
     print("Congratulations you have sold all the tickets")
